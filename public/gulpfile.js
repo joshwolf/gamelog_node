3f706b729293e1d145955aa5308337aa6e4704b0
Josh Wolf <josh.wolf@vml.com>
2017-01-24 10:41:16 -0500

    gulp

---
+++ public/gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp'),
+	plugins = require("gulp-load-plugins")({
+		pattern: ['gulp-*', 'gulp.*', 'main-bower-files'],
+		replaceString: /\bgulp[\-.]/
+	});
+
+// Define default destination folder
+var dest = 'app/';
+ 
+gulp.task('sass', function() {
+  gulp.src('app/styles/*.scss')
+    .pipe(plugins.sass())
+    .pipe(gulp.dest('app/styles/'))
+    .pipe(plugins.livereload({ start: true }));
+});
+ 
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('app/styles/*.scss', ['sass']);
+});
+
+gulp.task('default', function() {
+  gulp.src('app/styles/*.scss')
+    .pipe(plugins.sass())
+    .pipe(gulp.dest('app/styles/'))
+    .pipe(plugins.livereload({ start: true }));
+});
+
+gulp.task('bower', function() {
+	gulp.src(plugins.mainBowerFiles())
+		.pipe(plugins.filter('**/*.js'))
+		.pipe(plugins.concat('vendor.js'))
+//		.pipe(plugins.uglify())
+		.pipe(gulp.dest('app/scripts/'));
+
+	gulp.src(plugins.mainBowerFiles())
+		.pipe(plugins.filter('**/*.css'))
+		.pipe(plugins.concat('vendor.css'))
+		.pipe(gulp.dest('app/styles'));
+
+});
