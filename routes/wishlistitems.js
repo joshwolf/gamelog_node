acedf67ca2242e1eb3e9e348a930eee73983d357
Josh Wolf <josh.wolf@vml.com>
2017-01-30 23:24:05 -0500

    wishlist add success

--- routes/wishlistitems.js
+++ routes/wishlistitems.js
@@ -23,10 +23,8 @@ function loggedIn(req, res, next) {
 }
 
 router.get('/add/:game_id', loggedIn, function(req, res) {
-			console.log('eh');
 	models.WishlistLitem.findOrCreate({ where: { UserId: req.session.user.id, GameId: req.params.game_id }})
 		.spread(function(wishlistItem, created) {
-			console.log('foo');
 			res.jsonp({success: true});
 		});
 });
